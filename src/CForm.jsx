b8c92f633f0869308a4d766cb64277ec320ecf28
GAUTAM KUMAR PILLA <you@example.com>
2023-07-03 13:21:19 +0530

    latest

--- src/CForm.jsx
+++ src/CForm.jsx
@@ -1,7 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const CForm =()=> {
+  const [course, setCourse] = useState([]);
+
+  useEffect(()=>{
+      axios.get('http://localhost:4500/courses').then((res)=>{
+          setCourse([...res.data]);
+      })
+  },[]);
+  //console.log(course);
+
     const [addCourse, setAddCourse] = useState({
         name:'',
         duration:'',
@@ -16,9 +25,9 @@ const CForm =()=> {
   })
   const [patchCourse,setPatchCourse] = useState({
         name:'NodeJs',
-        // duration:'2weeks',
-        // type:'Offline',
-        // price: 10000
+        duration:'2weeks',
+        type:'Offline',
+        price: 10000
   })
 
   const handleSubmit=(event)=>{
@@ -56,8 +65,24 @@ const Patch=()=>{
 })
 }
 
+const DeleteRow = (e) =>{
+  const id = e.target.value;
+  axios.delete(`http://localhost:4500/deleteCourse/${id}`).then((res) =>{
+       console.log(res.data);
+  })
+  alert('Course deleted');
+}
+
+const EditRow=()=>{
+  const id = '649edd7fe1d7594fa7b8a8a9';
+    axios.patch(`http://localhost:4500/patchCourse/${id}`,patchCourse).then((res)=>{
+      console.log(res.data);
+})
+}
+
   return (
-    <div>
+    <div className='mb-3'>
+      <div>
        <form onSubmit={handleSubmit}> 
        <p>Name: </p>
        <input className="" type="text" placeholder="" onChange={(e) => {setAddCourse({...addCourse,name:(e.target.value)})}}/>
@@ -75,6 +100,38 @@ const Patch=()=>{
        <button onClick={Update} className='btn btn-warning ms-2'>Update Course</button>
        <button onClick={Patch} className='btn btn-info ms-2'>Patch Course</button>
     </div>
+
+    <div>
+      <table cellPadding={'10'} className="table table-bordered border-dark w-25 m-3">
+          <thead>
+              <tr>
+                  <th>Name</th>
+                  <th>Duration</th>
+                  <th>Type</th>
+                  <th>Price</th>
+                  <th></th>
+                  <th></th>
+              </tr>
+          </thead>
+          <tbody>
+              {
+                  course.map((cd)=>{
+                      return (
+                          <tr>
+                              <td>{cd.name}</td>
+                              <td>{cd.duration}</td>
+                              <td>{cd.type}</td>
+                              <td>{cd.price}</td>
+                              <td><button className="btn btn-success" onClick={EditRow}>Edit</button></td>
+                              <td><button value={cd._id} className="btn btn-dark p-1" onClick={DeleteRow}>Delete</button></td>
+                          </tr>
+                      )
+                  })
+              }
+          </tbody>
+      </table>
+  </div>
+  </div>
   )
 }
 
